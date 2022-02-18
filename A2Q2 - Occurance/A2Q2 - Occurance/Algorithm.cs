7309746ff04b23c8758857312fcec11846ddf12b
Vinicius Juliani Souza <58318997+Vinex101@users.noreply.github.com>
2022-02-18 13:00:39 +1300

    Add files via upload

---
+++ A2Q2 - Occurance/A2Q2 - Occurance/Algorithm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2Q2___Algorithm
+{
+    class Algorithm
+    { 
+        public void displayArray(int[] temparray)
+        {
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                Console.Write(temparray[i] + " ");
+            }
+        }
+
+        public int NumOccuranceSearch(int[] temparray, int tempvalue, int tempoccurance)
+        {
+            //The compiler does not like it when a return method has code paths that dont lead to a return, so I have these hard coded variables for the code to return if the value is not found.
+            int tempint = 0;
+            int tempindex = -1;
+
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                //Comparing the current element in array to the user value, then incrementing an int to count how many occurances of that value occur in the array.
+                if (temparray[i] == tempvalue)
+                {
+                    tempint++;
+                }
+
+                //Comparing the user occurance to the incremented occurance, if they are the same the amount of occurances is returned
+                if (tempoccurance == tempint)
+                {
+                    tempindex = i;
+                    return tempindex;
+                }
+            }
+            //This is here to make sure that code paths return, if the number is found then it will return that, if it is not it will return -1 which was hard coded in the beginning of the method.
+            return tempindex;
+
+
+        }
+    }
+}
---
+++ A2Q2 - Occurance/A2Q2 - Occurance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace A2Q2___Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Generator = new Random();
+            int[] NumArray = new int[100];
+            Algorithm NewAlgo = new Algorithm();
+            int value, occurance, index;
+
+            for (int i = 0; i < NumArray.Length; i++)
+            {
+                NumArray[i] = Generator.Next(1,101);
+            }
+
+            NewAlgo.displayArray(NumArray);
+
+            Console.Write("\nWhat value do you want to find: >");
+            value = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Which occurance do you want to find: >");
+            occurance = Convert.ToInt32(Console.ReadLine());
+
+            index = NewAlgo.NumOccuranceSearch(NumArray, value, occurance);
+
+            if (index == -1)
+            {
+                Console.WriteLine("Your value was not found in the array.");
+            }
+
+            else
+            {
+                Console.WriteLine("The item {0} has occurance {1} at index {2}", value, occurance, index);
+            }
+        
+
+
+            Console.ReadKey();
+        }
+    }
+}
