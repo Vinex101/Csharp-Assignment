2fdcfff33aa8a03496bef1c2e71221cff3d22e1b
Vinicius Juliani Souza <58318997+Vinex101@users.noreply.github.com>
2022-02-18 13:01:34 +1300

    Add files via upload

---
+++ A2Q3 - LastOccurance/A2Q3 - LastOccurance/Algorithm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2Q3___LastOccurance
+{
+    class Algorithm
+    {
+        public Algorithm()
+        { 
+        
+        }
+
+
+        public void displayArray(int[] temparray)
+        {
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                Console.Write(temparray[i] + " ");
+            }
+        }
+
+        public int LastOccuranceSearch(int[] temparray, int tempvalue)
+        {
+            //The compiler does not like it when a return method has code paths that dont lead to a return, so I have these hard coded variables for the code to return if the value is not found.
+            int index = -1;
+
+            //I used a backwards linear search to find the last occurance of the specified value in the array. It kinda feels cheesy to do it this way but it works everytime ¯\_(ツ)_/¯
+            for (int i = temparray.Length-1; i > -1; i--)
+            {
+                if (temparray[i] == tempvalue)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+            //This is here to make sure that code paths return, if the number is found then it will return that, if it is not it will return -1 which was hard coded in the beginning of the method.
+            return index;
+          
+        }
+
+    }
+}
---
+++ A2Q3 - LastOccurance/A2Q3 - LastOccurance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace A2Q3___LastOccurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Generator = new Random();
+            int[] NumArray = new int[20];
+            Algorithm NewAlgo = new Algorithm();
+            int uservalue;
+            int occurance;
+
+            for (int i = 0; i < NumArray.Length; i++)
+            {
+                NumArray[i] = Generator.Next(1,101);
+            }
+
+            Console.WriteLine("Original Array");
+            NewAlgo.displayArray(NumArray);
+
+            Console.Write("\n\nWhat Value would you like to find?: >");
+            uservalue = Convert.ToInt32(Console.ReadLine());
+
+            occurance = NewAlgo.LastOccuranceSearch(NumArray, uservalue);
+
+            if (occurance == -1)
+            {
+                Console.WriteLine("\nYour value of {0} was not found in the array." ,uservalue);
+            }
+
+            else
+            {
+                Console.WriteLine("\nThe item {0} has its last occurance at index {1}.", uservalue, occurance);
+            }
+
+            Console.Write("\nPress any key to quit...");
+
+            Console.ReadKey();
+        }
+    }
+}
